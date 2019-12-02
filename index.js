50596e508f8d93ecd0ca7140f8231f4e5afa2db6
Sobhit Kumar <sobhit.kumar@claritusconsulting.com>
2019-12-02 11:43:22 +0530

    Fix bugs

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const { Observable } = 'rxjs';
+const Rx = rxjs;
 let button = document.querySelector('button');
 // button.addEventListener('click', (e) => {
 //   console.log(e);
 // });
-Observable.fromEvent(button, 'click')
+console.log(Rx);
+Rx.fromEvent(button, 'click')
   .subscribe((e) => {
     console.log('Clicked')
   })
